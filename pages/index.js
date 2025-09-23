6d73f6f0815bd59f65dc10c8fe8203e25392c3ed
Mario Donato da Silva <donato.mario@gmail.com>
2025-09-23 08:42:18 +0000

    Message edited

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Ynha, u know I LOVE you right?! Right?! RIGHT?!</h1>
+    return <h1>Ynha, u know I LOVE you right?! Right?! RIGHT?! 🫰 </h1>
 }
 
 export default Home;
